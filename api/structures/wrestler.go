bea6d38c721f86e85da4f9b9b836ce53d4fc01b6
jcsix694 <jcsix694@gmail.com>
2022-06-01 12:00:32 +0100

    UPDATED wrestler struct

--- api/structures/wrestler.go
+++ api/structures/wrestler.go
@@ -2,12 +2,15 @@ package structures
 
 import (
 	"gorm.io/gorm"
+	"time"
 )
 
 type Wrestler struct {
-	Id   uint   `json:"id"`
-	Name string `json:"name"`
-	gorm.Model
+	ID        uint           `gorm:"primaryKey" json:"id"`
+	Name      string         `json:"name"`
+	CreatedAt time.Time      `gorm:"autoCreateTime:true" json:"createdAt"`
+	UpdatedAt time.Time      `gorm:"autoCreateTime:true" json:"updatedAt"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deletedAt"`
 }
 
 func (w *Wrestler) TableName() string {
